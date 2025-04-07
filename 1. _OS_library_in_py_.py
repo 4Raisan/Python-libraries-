4d5a88f0b8010466dc1649cb7c9ef97e726a7348
Rashen Anupama <mn4raisan@gmail.com>
2025-04-07 08:52:47 +0530

    Update 1. _OS_library_in_py_.py

--- 1. _OS_library_in_py_.py
+++ 1. _OS_library_in_py_.py
@@ -1,1 +1,5 @@
-
+# Script Location  # how to fined the files location
+'''
+- os.path.dirname(os.path.abspath(__file__)): Get the directory where the Python script is located.
+- __file__: Represents the relative path of the currently executing script. Use it with os.path functions to get the script's location.
+'''
