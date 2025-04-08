c6a330a8e368bf55dd54be6a645f4e8fe5782f12
Rashen Anupama <mn4raisan@gmail.com>
2025-04-08 23:53:41 +0530

    fix syntax error

--- 1. _OS_library_in_py_.py
+++ 1. _OS_library_in_py_.py
@@ -8,8 +8,8 @@ from os import *
 
 d = "4Raisan/Python-libraries-/1. _OS_library_in_py_.py" 
 # or use __file__ for use the script what we ran
-op = path.dirname(path.abspath(__file__)):
-op2 = path.dirname(path.abspath(d)):
+op = path.dirname(path.abspath(__file__))
+op2 = path.dirname(path.abspath(d))
 
 print(op)
 print()
